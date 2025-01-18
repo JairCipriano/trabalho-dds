b632730e8fa7bab424c022f6ee0195f436d162fb
JairCipriano <jaircipriano69@gmail.com>
2025-01-18 16:49:13 -0300

    e

--- dds.py
+++ dds.py
@@ -1,4 +1,4 @@
-from flask import Flask, Request, redirect, render_template
+from flask import Flask, request, redirect, render_template
 
 app =Flask(__name__)
 
@@ -6,11 +6,11 @@ app =Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/gerar-link', methods=['get'])
+@app.route('/gerar-link', methods=['GET'])
 
 def gerar_link():
-    matricula = Request.args.__get__('matricula', 'matricula padrao')
-    senha = Request.args.__get__('senha', 'senha padrao')
+    matricula = request.args.get('matricula', 'matricula padrao')
+    senha = request.args.get('senha', 'senha padrao')
 
     base_url = "https://forms.office.com/e/uNSqr31cfS"
     return redirect(f"{base_url}?matricula={matricula}&senha={senha}")
